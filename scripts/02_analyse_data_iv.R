b76617553ed9a9c1630d7e3f8834ef0eaf6e57ce
Samruddhi <samruddh.yerunkar@charite.de>
2023-09-05 16:11:33 +0200

    update enrollment variable according to new dataset

--- scripts/02_analyse_data_iv.R
+++ scripts/02_analyse_data_iv.R
@@ -11,6 +11,8 @@
 # - trial_days : The number of days trial was ongoing until termination
 # - enrollment_percentage : The percentage of enrollment achieved until termination
 
+library(dplyr)
+
 # load duration_of_enrollment function
 source(here::here("scripts", "functions", "duration_of_trial.R"))
 
@@ -22,7 +24,7 @@ source(here::here("scripts", "functions", "degree_of_enrollment.R"))
 # reading Intovalue raw data
 intovalue_raw <- rio::import("https://github.com/maia-sh/intovalue-data/blob/main/data/processed/trials.rds?raw=true")
 
-# prepare Intovalue data 
+# prepare Intovalue data with terminated trial study inclusion criteria  
 iv_terminated <- intovalue_raw %>%
   dplyr::filter(
     iv_completion,
@@ -35,19 +37,12 @@ iv_terminated <- intovalue_raw %>%
     registry == "ClinicalTrials.gov",
     # trials that are terminated
     recruitment_status == "Terminated"
-  ) %>%
-  # select required columns 
-  dplyr::select(
-    id, title, enrollment, start_date, primary_completion_date, completion_date, phase,
-    has_summary_results, is_prospective, masking, is_multicentric, has_publication,
-    pub_title, main_sponsor, intervention_type
-  )
+  ) 
 
-
-# use cthist to get historical data entry for iv terminated trials
-#iv_terminated_cthist <- clinicaltrials_gov_download(iv_terminated$id)
+# use cthist to get historical data entry for iv terminated trials (download date: 2023-08-02)
+#iv_terminated_cthist <- cthist::clinicaltrials_gov_download(iv_terminated$id)
 # note: information for two nct id couldnt be downloaded (NCT00150878,NCT01837082)
-iv_terminated_cthist <- read.csv(here::here("data","processed_dataset","intovalue", "cthist_iv.csv"))
+iv_terminated_cthist <- read.csv(here::here("data","processed","intovalue", "cthist_iv_1.csv"))
 
 
 # create reason for termination variable (refers to the reason of termination for a clinicial trial)------
@@ -72,7 +67,7 @@ iv_terminated_cthist <- iv_terminated_cthist %>%  dplyr::group_by(nctid) %>%
 # create a 'stop_date' variable (when trial overall status was first updated to terminated in registry)
 iv_terminated_cthist <- iv_terminated_cthist %>%
   dplyr::group_by(nctid) %>%
-  dplyr::mutate(stop_date = dplyr::if_else(overall_status == "Terminated", as.character(version_date), NA_character_),
+  dplyr::mutate(stop_date = dplyr::if_else(overall_status == "TERMINATED", as.character(version_date), NA_character_),
                 stop_date = min(stop_date, na.rm = TRUE)) %>%
   dplyr::ungroup()
 
@@ -83,14 +78,14 @@ iv_terminated_cthist <- iv_terminated_cthist %>%
 # create a 'anticipated enrollment' variable (refers to the expected number of participants that the trial aims to enroll)
 iv_terminated_cthist <- iv_terminated_cthist %>% 
   dplyr::group_by(nctid) %>% 
-  dplyr::mutate(anticipated_enrollment = ifelse(enrolment_type == "Anticipated", as.integer(enrolment), NA_integer_),
+  dplyr::mutate(anticipated_enrollment = ifelse(enrolment_type == "ESTIMATED", as.integer(enrolment), NA_integer_),
                 anticipated_enrollment = last(na.omit(anticipated_enrollment))) %>% 
   dplyr::ungroup()
 
 # create a 'actual enrollment' variable (refers to the observed number of participants who are actually enrolled)
 iv_terminated_cthist <- iv_terminated_cthist %>% 
   dplyr::group_by(nctid) %>% 
-  dplyr::mutate(actual_enrollment = ifelse(enrolment_type == "Actual", as.integer(enrolment), NA_integer_),
+  dplyr::mutate(actual_enrollment = ifelse(enrolment_type == "ACTUAL", as.integer(enrolment), NA_integer_),
                 actual_enrollment = last(actual_enrollment)) %>% 
   dplyr::ungroup()
 
@@ -116,3 +111,5 @@ iv_terminated_cthist_updated <- degree_of_enrollment(iv_terminated_cthist_update
 # generate duration of enrollment by using function 'duration_of_trial'
 iv_terminated_cthist_updated <- duration_of_trial(iv_terminated_cthist_updated, "start_date", "stop_date")
 
+# save dataset
+#write.csv(iv_terminated_cthist_updated,file = here::here("data", "processed", "intovalue", "iv_terminated_cthist_updated.csv"),row.names = FALSE)
